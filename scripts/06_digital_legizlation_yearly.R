15d0bc2ced915ac4fb4bd860046ac85be22dd8a9
bernhardeymann <b.eymann@bluewin.ch>
2024-05-24 11:36:42 +0200

    Adding to script 06 (counting digital legislation yearly): - sorting for Beginn.Geschäft dates - counting Beginn.Geschäft per year
    Creating seperate csv for Ende.Geschäft sorted count and Beginn.Geschäft sorted count

--- scripts/06_digital_legizlation_yearly.R
+++ scripts/06_digital_legizlation_yearly.R
@@ -8,15 +8,26 @@ library(dplyr)
 # load the data
 data <- read.csv("data_processed/ds_grosserrat_digital.csv")
 
-# convert the column "Ende.Geschäft" to a date
+# convert the columns "Beginn.Geschäft" and "Ende.Geschäft" to dates
+data$Beginn.Geschäft <- as.Date(data$Beginn.Geschäft, format = "%Y-%m-%d")
 data$Ende.Geschäft <- as.Date(data$Ende.Geschäft, format = "%Y-%m-%d")
 
-# sort the data by date
-data <- data %>% arrange(Ende.Geschäft)
+# sort the data by the "Ende.Geschäft" date
+data_end_sorted <- data %>% arrange(Ende.Geschäft)
 
-# count how many rows there are each year
-data %>%
+# count how many rows there are each year based on "Ende.Geschäft"
+data_end_sorted %>%
   mutate(year = year(Ende.Geschäft)) %>%
   group_by(year) %>%
   summarise(count = n()) %>%
-  write.csv("data_processed/ds_grosserrat_digital_yearly.csv", row.names = FALSE)
+  write.csv("data_processed/ds_grosserrat_digital_yearly_end.csv", row.names = FALSE)
+
+# sort the data by the "Beginn.Geschäft" date
+data_begin_sorted <- data %>% arrange(Beginn.Geschäft)
+
+# count how many rows there are each year based on "Beginn.Geschäft"
+data_begin_sorted %>%
+  mutate(year = year(Beginn.Geschäft)) %>%
+  group_by(year) %>%
+  summarise(count = n()) %>%
+  write.csv("data_processed/ds_grosserrat_digital_yearly_begin.csv", row.names = FALSE)
