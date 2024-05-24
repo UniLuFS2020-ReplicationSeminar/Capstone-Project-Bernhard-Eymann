6af79c9c24f37d359a18652102fcdb3ea83137a9
bernhardeymann <b.eymann@bluewin.ch>
2024-05-24 10:55:14 +0200

    updating script 5 to get the answers into the right order

--- scripts/05_counting_answers_umfrage.R
+++ scripts/05_counting_answers_umfrage.R
@@ -11,6 +11,9 @@ data <- read.csv("data/ds_umfrage.csv")
 # Defining possible answers (ranging from "sehr einverstanden" - agreeing very much - to "überhaupt nicht einverstanden" - not agreeing at all)
 answers <- c("sehr einverstanden", "eher einverstanden", "weiss nicht", "eher nicht einverstanden", "überhaupt nicht einverstanden")
 
+# Ensure the answers column is a factor with the specified levels
+data$bast7_2 <- factor(data$bast7_2, levels = answers)
+
 # Count answers in a long format
 answer_counts <- data %>%
   filter(bast7_2 %in% answers) %>%  # Ensure only relevant answers are counted
