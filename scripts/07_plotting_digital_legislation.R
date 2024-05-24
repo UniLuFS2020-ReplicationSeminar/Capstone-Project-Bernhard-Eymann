e45543434de14ba0c03acee21e3a28381c3603be
bernhardeymann <b.eymann@bluewin.ch>
2024-05-24 12:14:28 +0200

    Updating bar graph on legislative projects for displaying beginning projects, plotting

--- scripts/07_plotting_digital_legislation.R
+++ scripts/07_plotting_digital_legislation.R
@@ -5,15 +5,25 @@ rm(list=ls())
 library(ggplot2)
 library(dplyr)
 
-# load the data
-data <- read.csv("data_processed/ds_grosserrat_digital_yearly.csv")
+# load the 2 data sets
+data1 <- read.csv("data_processed/ds_grosserrat_digital_yearly_begin.csv")
+data2 <- read.csv("data_processed/ds_grosserrat_digital_yearly_end.csv")
+
+# Add a column to distinguish the datasets
+data1$stage <- "Beginning"
+data2$stage <- "End"
+
+# Combine the datasets
+combined_data <- rbind(data1, data2)
 
 # plot the data in a bar chart
-ggplot(data, aes(x = year, y = count)) +
-  geom_bar(stat = "identity") +
+ggplot(combined_data, aes(x = year, y = count, fill = stage)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_y_continuous(breaks = seq(0, 10, 1), limits = c(0, 10)) +  # Set the y-axis breaks to integers and limit to a maximum of 10
   labs(title = "Legislation in the Grand Council of Basel-Stadt related to digitalization",
        x = "Year",
-       y = "Number of legislation projects ended related to digitalization") +
+       y = "Number of legislation projects related to digitalization",
+       fill = "Stage") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
