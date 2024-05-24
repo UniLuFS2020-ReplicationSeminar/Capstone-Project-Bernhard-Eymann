a2597ba40930534f59d830932b24b69ca4196a37
bernhardeymann <b.eymann@bluewin.ch>
2024-05-24 11:19:55 +0200

    Translating labels in pie chart to english, replotting

--- scripts/08_plotting_survey.R
+++ scripts/08_plotting_survey.R
@@ -5,6 +5,7 @@ rm(list=ls())
 library(ggplot2)
 library(dplyr)
 library(scales)
+library(RColorBrewer)
 
 # load the data
 data <- read.csv("data_processed/umfrage_answer_counts.csv")
@@ -13,6 +14,7 @@ str(data)
 
 # Defining possible answers in the desired order
 answers <- c("sehr einverstanden", "eher einverstanden", "weiss nicht", "eher nicht einverstanden", "überhaupt nicht einverstanden")
+english_labels <- c("Strongly Agree", "Agree", "Don't Know", "Disagree", "Strongly Disagree")
 
 # Ensure the answers column is a factor with the specified levels
 data$answer <- factor(data$answer, levels = answers)
@@ -25,15 +27,16 @@ data <- data %>%
 ggplot(data, aes(x = "", y = count, fill = answer)) +
   geom_bar(stat = "identity", width = 1) +
   coord_polar("y") +
-  labs(title = '2020 public survey in the canton Basel-Stadt:\n"Do you want Basel-Stadt to be a pioneering canton in digitalization?"',
+  labs(title = '2020 survey in the canton Basel-Stadt:\n"Do you want Basel-Stadt to be a pioneering canton in digitalization?"',
        x = "",
        y = "") +
-  scale_fill_brewer(palette = "Set3") +  # Use a color palette from RColorBrewer
+  scale_fill_manual(values = brewer.pal(length(answers), "Set3"), 
+                    labels = english_labels, 
+                    name = "Answer") +  # Use manual fill scale with English labels
   theme_void() +  # Use a void theme for a cleaner look
   theme(
     plot.title = element_text(hjust = 0.5, face = "bold"),
     legend.position = "right",
-    legend.title = element_blank(),
     plot.background = element_rect(fill = "white", color = NA),  # Set the background to white
     panel.background = element_rect(fill = "white", color = NA)  # Set the panel background to white
   ) +
@@ -42,4 +45,3 @@ ggplot(data, aes(x = "", y = count, fill = answer)) +
 
 # save the plot
 ggsave("plots/umfrage_answer_counts.png", width = 10, height = 6, dpi = 300)
-
