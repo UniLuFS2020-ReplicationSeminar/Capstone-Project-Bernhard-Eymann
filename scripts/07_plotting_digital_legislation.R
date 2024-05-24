e3b9670773828ef0845dc6bc7acbfe7fb4469e1c
bernhardeymann <b.eymann@bluewin.ch>
2024-05-24 11:11:37 +0200

    Updating labels in legizlation bar chart script, updating plot

--- scripts/07_plotting_digital_legislation.R
+++ scripts/07_plotting_digital_legislation.R
@@ -11,9 +11,9 @@ data <- read.csv("data_processed/ds_grosserrat_digital_yearly.csv")
 # plot the data in a bar chart
 ggplot(data, aes(x = year, y = count)) +
   geom_bar(stat = "identity") +
-  labs(title = "Digital legislation in the Grand Council of Basel-Stadt",
+  labs(title = "Legislation in the Grand Council of Basel-Stadt related to digitalization",
        x = "Year",
-       y = "Number of digital legislation projects ended") +
+       y = "Number of legislation projects ended related to digitalization") +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
